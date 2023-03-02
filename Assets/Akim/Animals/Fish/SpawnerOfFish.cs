d6e41efd21ab5e3ef7146b334f1681fe5740b493
Akim Zuev <>
2023-03-03 09:40:06 +1000

    Fish.2
    Полнная доработка рыб.

--- Assets/Akim/Animals/Fish/SpawnerOfFish.cs
+++ Assets/Akim/Animals/Fish/SpawnerOfFish.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 
 public class SpawnerOfFish : MonoBehaviour
 {
-    public List<GameObject> fishPrefabs;
-    public GameObject Plot;
+    [SerializeField]
+    List<GameObject> FishPrefabs;
+    [SerializeField]
+    GameObject Plot;
+    [SerializeField]
+    List<GameObject> Waypoints;
 
     private void Update() // временно, удалить. Для вызова функции, позже удалить!!!!
     {
         FishSpawn();
     }   
-
+        
     void FishSpawn()
     {
         float plotz = Plot.transform.position.z; // узнаем значения плота по оси z
         
         if (GameObject.FindGameObjectsWithTag("Fish").Count() < 2) // проверяем сколько рыб находится на сцене
         {
-            int randomIndex = Random.Range(0, fishPrefabs.Count); // берем рандомную рыбу
-            GameObject randomFish = Instantiate(fishPrefabs[randomIndex], new Vector3(2, 0, plotz), Quaternion.identity); // спавним рандомную рыбу, справа от плота
+            int randomIndex = Random.Range(0, FishPrefabs.Count); // берем рандомную рыбу
+            GameObject randomFish = Instantiate(FishPrefabs[randomIndex], new Vector3(2, 0, plotz), Quaternion.identity); // спавним рандомную рыбу, справа от плота
+            randomFish.transform.parent = Plot.transform;
+            randomFish.GetComponent<PatrolFish>().waypoints = Waypoints; 
         }
-
+            
     }
 
 }
